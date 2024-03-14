9610140bc61a4f847796df1ca679a0d67898294b
javeriagithub <163407795+javeriagithub@users.noreply.github.com>
2024-03-14 16:22:18 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log("hello world");
+console.log("hello back to my whatapp channel");
+console.log("thankyou kamran khan tasstori,sir zia for giving us course");
+
+let message1 = "hello world";
+console.log(message1);
+ let message2 = "hello ai plannet";
+console.log(message2);
+
+let alisha = "you will playing tennis tamorrow";
+console.log(alisha);
+let students = "All classes will delay tomorrow";
+console.log(students);
